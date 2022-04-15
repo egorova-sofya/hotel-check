71a5de13c6972f1bb6bd74b9081e4cf0d58fd80c
egorova-sofya <egorovaSjob@yandex.ru>
2022-04-15 16:39:54 +0300

    changed name of folder

--- src/components/FeaturedHotels/FeaturedHotels.js
+++ src/components/FeaturedHotels/FeaturedHotels.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Title from "../Title/Title";
 import FeaturedHotelsFilter from "./FeaturedHotelsFilter/FeaturedHotelsFilter";
-import FeaturedHotelsItem from "./FeaturedHotelsItem/FeaturedHotelsItem";
 
 const FeaturedHotels = () => {
   return (
     <>
       <Title weight="bold">Избранное</Title>
       <FeaturedHotelsFilter />
-      <FeaturedHotelsItem />
     </>
   );
 };
--- src/components/FeaturedHotels/FeaturedHotelsFilter/FeaturedHotelsFilter.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FeaturedHotelsFilter = () => {
-  return <div>FeaturedHotelsFilter</div>;
-};
-
-export default FeaturedHotelsFilter;
